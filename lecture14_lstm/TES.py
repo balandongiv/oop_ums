fa82cf716ac4c870f01d4a91125860e38adf1224
rpb <balandongiv@gmail.com>
2023-10-28 10:24:33 +0800

    Refactoring folder to reflect changes made in Table4

--- lecture14_lstm/TES.py
+++ lecture14_lstm/TES.py
@@ -1,173 +1,173 @@
-import torch
-
-import os
-import numpy as np
-import pandas as pd
-from tqdm import tqdm
-import seaborn as sns
-from pylab import rcParams
-import matplotlib.pyplot as plt
-from matplotlib import rc
-from sklearn.preprocessing import MinMaxScaler
-from pandas.plotting import register_matplotlib_converters
-from torch import nn, optim
-
-%matplotlib inline
-%config InlineBackend.figure_format='retina'
-
-sns.set(style='whitegrid', palette='muted', font_scale=1.2)
-
-HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#93D30C", "#8F00FF"]
-
-sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
-
-rcParams['figure.figsize'] = 14, 10
-register_matplotlib_converters()
-
-RANDOM_SEED = 42
-np.random.seed(RANDOM_SEED)
-torch.manual_seed(RANDOM_SEED)
-
-!gdown --id 1AsfdLrGESCQnRW5rbMz56A1KBc3Fe5aV
-
-df = pd.read_csv('time_series_19-covid-Confirmed.csv')
-df.head()
-
-df = df.iloc[:, 4:]
-
-daily_cases = df.sum(axis=0)
-daily_cases.index = pd.to_datetime(daily_cases.index)
-daily_cases.head()
-
-daily_cases = daily_cases.diff().fillna(daily_cases[0]).astype(np.int64)
-daily_cases.head()
-
-
-test_data_size = 14
-
-train_data = daily_cases[:-test_data_size]
-test_data = daily_cases[-test_data_size:]
-
-train_data.shape
-
-scaler = MinMaxScaler()
-
-scaler = scaler.fit(np.expand_dims(train_data, axis=1))
-
-train_data = scaler.transform(np.expand_dims(train_data, axis=1))
-
-test_data = scaler.transform(np.expand_dims(test_data, axis=1))
-
-def create_sequences(data, seq_length):
-    xs = []
-    ys = []
-
-    for i in range(len(data)-seq_length-1):
-        x = data[i:(i+seq_length)]
-        y = data[i+seq_length]
-        xs.append(x)
-        ys.append(y)
-
-    return np.array(xs), np.array(ys)
-
-seq_length = 5
-X_train, y_train = create_sequences(train_data, seq_length)
-X_test, y_test = create_sequences(test_data, seq_length)
-
-X_train = torch.from_numpy(X_train).float()
-y_train = torch.from_numpy(y_train).float()
-
-X_test = torch.from_numpy(X_test).float()
-y_test = torch.from_numpy(y_test).float()
-
-class CoronaVirusPredictor(nn.Module):
-
-    def __init__(self, n_features, n_hidden, seq_len, n_layers=2):
-        super(CoronaVirusPredictor, self).__init__()
-
-        self.n_hidden = n_hidden
-        self.seq_len = seq_len
-        self.n_layers = n_layers
-
-        self.lstm = nn.LSTM(
-            input_size=n_features,
-            hidden_size=n_hidden,
-            num_layers=n_layers,
-            dropout=0.5
-        )
-
-        self.linear = nn.Linear(in_features=n_hidden, out_features=1)
-
-    def reset_hidden_state(self):
-        self.hidden = (
-            torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
-            torch.zeros(self.n_layers, self.seq_len, self.n_hidden)
-        )
-
-    def forward(self, sequences):
-        lstm_out, self.hidden = self.lstm(
-            sequences.view(len(sequences), self.seq_len, -1),
-            self.hidden
-        )
-        last_time_step = \
-            lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
-        y_pred = self.linear(last_time_step)
-        return y_pred
-
-    def train_model(
-            model,
-            train_data,
-            train_labels,
-            test_data=None,
-            test_labels=None
-    ):
-loss_fn = torch.nn.MSELoss(reduction='sum')
-
-optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
-num_epochs = 60
-
-train_hist = np.zeros(num_epochs)
-test_hist = np.zeros(num_epochs)
-
-for t in range(num_epochs):
-    model.reset_hidden_state()
-
-    y_pred = model(X_train)
-
-    loss = loss_fn(y_pred.float(), y_train)
-
-    if test_data is not None:
-        with torch.no_grad():
-            y_test_pred = model(X_test)
-            test_loss = loss_fn(y_test_pred.float(), y_test)
-        test_hist[t] = test_loss.item()
-
-        if t % 10 == 0:
-            print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
-    elif t % 10 == 0:
-        print(f'Epoch {t} train loss: {loss.item()}')
-
-    train_hist[t] = loss.item()
-
-    optimiser.zero_grad()
-
-    loss.backward()
-
-    optimiser.step()
-
-return model.eval(), train_hist, test_hist
-
-
-model = CoronaVirusPredictor(
-    n_features=1,
-    n_hidden=512,
-    seq_len=seq_length,
-    n_layers=2
-)
-model, train_hist, test_hist = train_model(
-    model,
-    X_train,
-    y_train,
-    X_test,
-    y_test
-)
+# # import torch
+# #
+# # import os
+# # import numpy as np
+# # import pandas as pd
+# # from tqdm import tqdm
+# # import seaborn as sns
+# # from pylab import rcParams
+# # import matplotlib.pyplot as plt
+# # from matplotlib import rc
+# # from sklearn.preprocessing import MinMaxScaler
+# # from pandas.plotting import register_matplotlib_converters
+# # from torch import nn, optim
+# #
+# # %matplotlib inline
+# # %config InlineBackend.figure_format='retina'
+#
+# sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+#
+# HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#93D30C", "#8F00FF"]
+#
+# sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+#
+# rcParams['figure.figsize'] = 14, 10
+# register_matplotlib_converters()
+#
+# RANDOM_SEED = 42
+# np.random.seed(RANDOM_SEED)
+# torch.manual_seed(RANDOM_SEED)
+#
+# !gdown --id 1AsfdLrGESCQnRW5rbMz56A1KBc3Fe5aV
+#
+# df = pd.read_csv('time_series_19-covid-Confirmed.csv')
+# df.head()
+#
+# df = df.iloc[:, 4:]
+#
+# daily_cases = df.sum(axis=0)
+# daily_cases.index = pd.to_datetime(daily_cases.index)
+# daily_cases.head()
+#
+# daily_cases = daily_cases.diff().fillna(daily_cases[0]).astype(np.int64)
+# daily_cases.head()
+#
+#
+# test_data_size = 14
+#
+# train_data = daily_cases[:-test_data_size]
+# test_data = daily_cases[-test_data_size:]
+#
+# train_data.shape
+#
+# scaler = MinMaxScaler()
+#
+# scaler = scaler.fit(np.expand_dims(train_data, axis=1))
+#
+# train_data = scaler.transform(np.expand_dims(train_data, axis=1))
+#
+# test_data = scaler.transform(np.expand_dims(test_data, axis=1))
+#
+# def create_sequences(data, seq_length):
+#     xs = []
+#     ys = []
+#
+#     for i in range(len(data)-seq_length-1):
+#         x = data[i:(i+seq_length)]
+#         y = data[i+seq_length]
+#         xs.append(x)
+#         ys.append(y)
+#
+#     return np.array(xs), np.array(ys)
+#
+# seq_length = 5
+# X_train, y_train = create_sequences(train_data, seq_length)
+# X_test, y_test = create_sequences(test_data, seq_length)
+#
+# X_train = torch.from_numpy(X_train).float()
+# y_train = torch.from_numpy(y_train).float()
+#
+# X_test = torch.from_numpy(X_test).float()
+# y_test = torch.from_numpy(y_test).float()
+#
+# class CoronaVirusPredictor(nn.Module):
+#
+#     def __init__(self, n_features, n_hidden, seq_len, n_layers=2):
+#         super(CoronaVirusPredictor, self).__init__()
+#
+#         self.n_hidden = n_hidden
+#         self.seq_len = seq_len
+#         self.n_layers = n_layers
+#
+#         self.lstm = nn.LSTM(
+#             input_size=n_features,
+#             hidden_size=n_hidden,
+#             num_layers=n_layers,
+#             dropout=0.5
+#         )
+#
+#         self.linear = nn.Linear(in_features=n_hidden, out_features=1)
+#
+#     def reset_hidden_state(self):
+#         self.hidden = (
+#             torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
+#             torch.zeros(self.n_layers, self.seq_len, self.n_hidden)
+#         )
+#
+#     def forward(self, sequences):
+#         lstm_out, self.hidden = self.lstm(
+#             sequences.view(len(sequences), self.seq_len, -1),
+#             self.hidden
+#         )
+#         last_time_step = \
+#             lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
+#         y_pred = self.linear(last_time_step)
+#         return y_pred
+#
+#     def train_model(
+#             model,
+#             train_data,
+#             train_labels,
+#             test_data=None,
+#             test_labels=None
+#     ):
+# loss_fn = torch.nn.MSELoss(reduction='sum')
+#
+# optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+# num_epochs = 60
+#
+# train_hist = np.zeros(num_epochs)
+# test_hist = np.zeros(num_epochs)
+#
+# for t in range(num_epochs):
+#     model.reset_hidden_state()
+#
+#     y_pred = model(X_train)
+#
+#     loss = loss_fn(y_pred.float(), y_train)
+#
+#     if test_data is not None:
+#         with torch.no_grad():
+#             y_test_pred = model(X_test)
+#             test_loss = loss_fn(y_test_pred.float(), y_test)
+#         test_hist[t] = test_loss.item()
+#
+#         if t % 10 == 0:
+#             print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
+#     elif t % 10 == 0:
+#         print(f'Epoch {t} train loss: {loss.item()}')
+#
+#     train_hist[t] = loss.item()
+#
+#     optimiser.zero_grad()
+#
+#     loss.backward()
+#
+#     optimiser.step()
+#
+# return model.eval(), train_hist, test_hist
+#
+#
+# model = CoronaVirusPredictor(
+#     n_features=1,
+#     n_hidden=512,
+#     seq_len=seq_length,
+#     n_layers=2
+# )
+# model, train_hist, test_hist = train_model(
+#     model,
+#     X_train,
+#     y_train,
+#     X_test,
+#     y_test
+# )
